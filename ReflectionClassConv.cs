d56574e4766b8c2ee33afa2afdf24e4dd2227f37
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2017-08-27 02:30:00 +0300

    Reflection Class Converter cs-file added

---
+++ ReflectionClassConv.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace ReflectionClassConverter
+{
+    class ReflectionClassConv
+    {
+        static Target ConvertClass(Source sourceInstance)
+        {
+            Target targetInstance = new Target();
+
+            var SourceFields = typeof(Source).GetProperties(); 
+            var TargetFields = typeof(Target).GetProperties();
+
+            try
+            {
+                foreach (var sourceItem in SourceFields)
+                {
+                    foreach (var targetItem in TargetFields)
+                    {
+                        if(sourceItem.Name == targetItem.Name)
+                        {
+                            targetItem.SetValue(targetInstance, sourceItem.GetValue(sourceInstance));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return targetInstance;
+        }
+
+        static void Main(string[] args)
+        {
+            Source sourceInstance = new Source() { MyProperty = 1, MyProperty2 = 2, MyProperty3 = 3};
+            Target TargetResult = ConvertClass(sourceInstance);
+
+            Console.WriteLine(TargetResult.MyProperty);
+            Console.WriteLine(TargetResult.MyProperty2);
+            Console.WriteLine(TargetResult.MyProperty3);
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+    class Source
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+    }
+
+    class Target
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+    }
+}
